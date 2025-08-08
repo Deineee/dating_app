6a9d345b5e300e7fb85c93986eeb552ca0ba9856
Deineee <deinepomperada@gmail.com>
2025-08-09 02:53:40 +0800

    Initial commit

---
+++ app/javascript/entrypoints/application.js
@@ -0,0 +1,18 @@
+import { createApp, h, provide } from 'vue'
+import App from '../src/App.vue'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+const apolloClient = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+})
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render: () => h(App)
+})
+
+app.mount('#app')
---
+++ app/javascript/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Dating App</h1>
+    <p>Rails + Vue + Apollo is working!</p>
+  </div>
+</template>
+
+<script setup>
+// future Apollo queries go here
+</script>
