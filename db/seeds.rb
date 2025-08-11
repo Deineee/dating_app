be2679277e4989f6e0eeacc3fec95d016cc02362
Deineee <deinepomperada@gmail.com>
2025-08-11 22:09:38 +0800

    added admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+User.create!(
+  email: "admin@example.com",
+  password: "password123",
+  password_confirmation: "password123",
+  first_name: "Admin",
+  last_name: "User",
+  mobile_number: "0987654321",
+  birthdate: Date.new(1990, 1, 1),
+  gender: "Male",
+  sexual_orientation: "Male",
+  gender_interest: "Female",
+  country: "Philippines",
+  state: "Metro Manila",
+  city: "Manila",
+  school: "Admin School",
+  bio: "I am the admin user.",
+  role: "admin",
+  primary_photo: "https://res.cloudinary.com/dt53bls5a/image/upload/v1754905921/2osnzobnkhlhglm8tmk0q0y4uxb0.jpg"
+)
